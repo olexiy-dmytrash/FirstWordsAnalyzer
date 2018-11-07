3da2b844a176ba540c5c4765b92252cbf75bbc80
Olexiy Dmytrash <olexiy.dmytrash@gmail.com>
2018-11-07 19:25:00 +0200

    Words

---
+++ FirstWordsAnalyzer/Controllers/WordsController.cs
@@ -0,0 +1,128 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using FirstWordsAnalyzer.Models;
+
+namespace FirstWordsAnalyzer.Controllers
+{
+    public class WordsController : Controller
+    {
+        private FirstWordsAnalyzerEntities db = new FirstWordsAnalyzerEntities();
+
+        // GET: Words
+        public async Task<ActionResult> Index()
+        {
+            return View(await db.Words.ToListAsync());
+        }
+
+        // GET: Words/Details/5
+        public async Task<ActionResult> Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Word word = await db.Words.FindAsync(id);
+            if (word == null)
+            {
+                return HttpNotFound();
+            }
+            return View(word);
+        }
+
+        // GET: Words/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Words/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<ActionResult> Create([Bind(Include = "Id,Text,FirstTranslationVariant,SecondTranslationVariant,ThirdTranslationVariant,NoSense,NotBasic")] Word word)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Words.Add(word);
+                await db.SaveChangesAsync();
+                return RedirectToAction("Index");
+            }
+
+            return View(word);
+        }
+
+        // GET: Words/Edit/5
+        public async Task<ActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Word word = await db.Words.FindAsync(id);
+            if (word == null)
+            {
+                return HttpNotFound();
+            }
+            return View(word);
+        }
+
+        // POST: Words/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<ActionResult> Edit([Bind(Include = "Id,Text,FirstTranslationVariant,SecondTranslationVariant,ThirdTranslationVariant,NoSense,NotBasic")] Word word)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(word).State = EntityState.Modified;
+                await db.SaveChangesAsync();
+                return RedirectToAction("Index");
+            }
+            return View(word);
+        }
+
+        // GET: Words/Delete/5
+        public async Task<ActionResult> Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Word word = await db.Words.FindAsync(id);
+            if (word == null)
+            {
+                return HttpNotFound();
+            }
+            return View(word);
+        }
+
+        // POST: Words/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<ActionResult> DeleteConfirmed(int id)
+        {
+            Word word = await db.Words.FindAsync(id);
+            db.Words.Remove(word);
+            await db.SaveChangesAsync();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
