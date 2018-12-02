76b6cbc084b36bf051293d85cc3c5c554c6a35b5
Olexiy Dmytrash <olexiy.dmytrash@gmail.com>
2018-12-02 11:41:52 +0200

    Books test

---
+++ FirstWordsAnalyzer.Tests/Controllers/BooksControllerTest.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Threading.Tasks;
+using System.Web.Mvc;
+using FirstWordsAnalyzer.Controllers;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace FirstWordsAnalyzer.Tests.Controllers
+{
+    [TestClass]
+    public class BooksControllerTest
+    {
+        [TestMethod]
+        public void Index()
+        {
+            // Arrange
+            BooksController controller = new BooksController();
+
+            // Act
+            ViewResult result = controller.Index().Result as ViewResult;
+
+            // Assert
+            Assert.IsNotNull(result);
+        }
+    }
+}
