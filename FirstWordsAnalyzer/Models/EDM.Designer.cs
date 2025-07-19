97fe4c98abcb706d142ad9f1c9a0f6c8d26df890
olexiy-dmytrash <olexiy.dmytrash@gmail.com>
2025-07-19 18:06:49 +0300

    Fixed

--- FirstWordsAnalyzer/Models/EDM.Designer.cs
+++ FirstWordsAnalyzer/Models/EDM.Designer.cs
@@ -1,4 +1,4 @@
-﻿// T4 code generation is enabled for model 'C:\Projects\FirstWordsAnalyzer\FirstWordsAnalyzer\FirstWordsAnalyzer\Models\EDM.edmx'. 
+﻿// T4 code generation is enabled for model 'D:\git\FirstWordsAnalyzer\FirstWordsAnalyzer\Models\EDM.edmx'. 
 // To enable legacy code generation, change the value of the 'Code Generation Strategy' designer
 // property to 'Legacy ObjectContext'. This property is available in the Properties Window when the model
 // is open in the designer.
